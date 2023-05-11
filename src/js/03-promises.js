725e6d5867259d954d923de7b032afbb211306f6
Alex_N <120394126+Sanya486@users.noreply.github.com>
2023-05-11 13:12:47 +0300

    Test

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,5 @@
+const alex = 0;
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
@@ -5,4 +7,4 @@ function createPromise(position, delay) {
   } else {
     // Reject
   }
-}
+
